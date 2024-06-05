d1abc879c781e0ad189fbe272e7dd297e462adf8
klingenbergh <151225560+klingenbergh@users.noreply.github.com>
2024-06-05 22:29:36 +0200

    Delete src/credentials.h
    e

--- src/credentials.h
+++
@@ -1,6 +0,0 @@
-// Enter your wifi details here
-const char *ssid = "*********";
-const char *password = "**********";
-
-// Enter your KlingShell server here
-const char *server = "http://IPORDNS:10000/KlingShell";
