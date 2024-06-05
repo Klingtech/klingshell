cfe637400fd31ca2f8675c1b828cbe2428dc8c35
klingenbergh <151225560+klingenbergh@users.noreply.github.com>
2024-06-05 21:55:29 +0200

    Create credentials.h

---
+++ src/credentials.h
@@ -0,0 +1,6 @@
+// Enter your wifi details here
+const char *ssid = "*********";
+const char *password = "**********";
+
+// Enter your KlingShell server here
+const char *server = "http://IPORDNS:10000/KlingShell";
